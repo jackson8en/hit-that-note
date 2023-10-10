d7ccccf3a886efb1e6631f942d5e3e45e55b2169
Ben Jackson <Ben@JacksonBR.com>
2023-10-10 18:50:29 +0100

    tweak change

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="flex justify-center items-center min-h-screen py-10">
       <div className="flex min-h-fit w-[80%] flex-col items-center">
         <h1 className="text-6xl font-semibold mb-3">Hit that <span className="text-accent font-extrabold">NOTE</span></h1>
-        <p>Under Construction</p>
+        <p className="text-xl">Under Construction</p>
       </div>
     </main>
   )
